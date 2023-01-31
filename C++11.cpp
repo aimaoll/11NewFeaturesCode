3bb57a3244f1dc3b90112fd7da10dfe19e32b158
luohonglei <luohonglei@longtek.net>
2023-01-31 16:53:10 +0800

    2023.1.31

--- C++11.cpp
+++ C++11.cpp
@@ -2,6 +2,8 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int size()
 {
@@ -20,6 +22,7 @@ constexpr int j = 0;
 
 int main()
 {
+    /* long long类型 */
     if (0)
     {
         short a = 0;
@@ -36,7 +39,8 @@ int main()
         std::cout << "sizeof(float)=" << sizeof(e) << "byte" << std::endl;
         std::cout << "sizeof(double)=" << sizeof(f) << "byte" << std::endl;
     }
-    
+
+    /* 列表初始化 */
     if (0)
     {
         /* 初始化不是赋值
@@ -56,6 +60,7 @@ int main()
         std::cout << "c = " << c << ",d = " << d << std::endl;
     }
 
+    /* nullptr常量 */
     if (0)
     {
         /* C++11新标准引入nullptr,是一种特殊类型的字面值,它可被转换成任意其他的指针类型
@@ -69,6 +74,7 @@ int main()
         */
     }
 
+    /* constexpr变量 */
     if (0)
     {
         /* 常量表达式 是指不会改变并且在编译过程就能得到计算结果的表达式
@@ -87,6 +93,7 @@ int main()
         constexpr const int* p2 = &j; // p2是常量指针,指向整数常量j
     }
 
+    /* 类型别名声明 */
     if (0)
     {
         // 类型别名 是一个名字,它是某种类型的同义词
@@ -98,6 +105,7 @@ int main()
         LD a = 0; // 等价于long double a = 0;
     }
 
+    /* auto类型指示符 */
     if (0)
     {
         // C++11新标准引入auto类型说明符,让编译器通过初始值来推断变量的类型,显然auto定义的变量必须有初始值
@@ -108,15 +116,17 @@ int main()
         //auto j = 0, k = 0.01; error:j和k类型不一致
     }
 
+    /* decltype类型指示符 */
     if (0)
     {
         // decltype:选择并返回操作数的数据类型,编译器分析表达式并得到它的类型,却不实际计算表达式的值
-        float x = 12.34;
+        double x = 12.34;
         decltype(size()) sum = x; // sum的类型就是函数size的返回类型
         std::cout << sum;
     }
 
-    if (1)
+    /* 类内初始化 */
+    if (0)
     {
         /* C++11新标准规定,可以为数据成员提供一个类内初始值
         *  创建对象时,类内初始值将用于初始化数据成员,没有初始值的成员将被默认初始化
@@ -127,6 +137,103 @@ int main()
         // C++11新标准允许编译器通过auto或decltype来推断变量的类型
         std::string b = "1";
         auto c = b.size(); // c的类型是std::string::size_type
+    }
+
+    /* 范围for语句 */
+    if (0)
+    {
+        // C++11新标准提供 范围for语句,这种语句遍历给定序列中的每个元素并对序列中的每个值执行某种操作
+        string str("some string");
+        for (auto c : str) // 对于str中的每个字符
+        {
+            cout << c << endl; // 输出当前字符
+        }
+
+        string s("Hello World!!!");
+        decltype(s.size()) punct_cnt = 0; // punct_cnt的类型和s.size的返回类型一样,也就是string::size_type
+        for (auto c : s) // 对于s中的每个字符
+        {
+            if (ispunct(c)) // 如果该字符是标点符号
+            {
+                ++punct_cnt; // 将标点符号的计数加1
+            }
+        }
+        cout << "punct_cun=" << punct_cnt << endl;
+
+        for (auto& c : s) // 对于s中的每个字符(注意c是引用)
+        {
+            c = toupper(c); // c是一个引用,因此赋值语句将改变s中字符的值
+        }
+        cout << s << endl;
+    }
+
+    /* 定义vector对象的vector(向量的向量) */
+    /* vector对象的列表初始化 */
+    if (0)
+    {
+        // 早期版本C++标准中如果vector的元素还是vector(或其他模板类型),则需要写为vector<vector<int> >
+        vector<vector<int> > a; // 早期版本C++标准
+        vector<vector<int>> b; // C++11新标准
+
+        // C++11新标准提供了列表初始化的方法为vector对象的元素进行赋初值
+        vector<string> c{ "a", "aa", "aaa" };
+        vector<string> d = { "a", "aa", "aaa" };
+        for (auto str : c)
+        {
+            cout << str << endl;
+        }
+    }
+
+    /* 容器的cbegin和cend函数 */
+    if (0)
+    {
+        // begin和end返回的具体类型由对象是否是常量决定,如果是常量:const_iterator,如果不是常量:iterator
+        vector<int> i;
+        const vector<int>j;
+        auto it1 = i.begin(); // it1的类型是vector<int>::iterator
+        auto it2 = j.begin(); // it2的类型是vector<int>::const_iterator
+        // C++11新标准引入cbegin和cend函数,不论对象本身是否为常量,返回值都是const_iterator
+        auto it3 = i.cbegin(); // it3的类型是vector<int>::const_iterator
+    }
+
+    /* 标准库的begin和end函数 */
+    if (0)
+    {
+        // C++11新标准引入了begin和end函数,与容器中的两个同名成员功能类似,不过数组不是类类型,故此函数使用形式是将数组作为他们的参数
+        int ia[] = { 0, 1, 2 };
+        int* beg = begin(ia);
+        int* last = end(ia);
+        for (; beg != last; ++beg)
+        {
+            cout << *beg << endl;
+        }
+    }
+
+    /* 使用auto和decltype简化声明 */
+    if (0)
+    {
+        int ia[3][4] = {0}; // 大小为3的数组,每个元素是含有4个整数的数组
+        int(*p)[4] = ia; // p指向含有4个整数的数组
+        p = &ia[2]; // p指向ia的尾元素,等同于p = ia + 2;
+        
+        /* C++11新标准使得通过auto或decltype就能尽可能避免在数组前面加上一个指针类型
+        *  虽然能推断出p的类型是指向含有4个整数的数组的指针,q的类型是指向整数的指针
+        *  但是使用auto关键字后就不必烦心这些类型到底是什么了
+        */
+        for (auto p = begin(ia); p != end(ia); ++p)
+        {
+            for (auto q = begin(*p); q != end(*p); ++q)
+            {
+                cout << *q << ' ';
+            }
+            cout << endl;
+        }
+    }
+
+    /* 除法的舍入规则 */
+    if (1)
+    {
+
     }
 
     return 0;
